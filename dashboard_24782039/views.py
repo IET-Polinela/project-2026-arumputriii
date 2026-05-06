f436a37083b0a2e5701e16e2121e5711d6f08ce3
arumputriii <arummputriiww@gmail.com>
2026-05-06 15:29:45 +0700

    CHECKPOINT 6: Implementasi Dashboard Statistik & Chart JS

--- dashboard_24782039/views.py
+++ dashboard_24782039/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
+from django.http import JsonResponse
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+from main_app.models import Barang
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard_24782039/dashboard.html'
+
+
+class DashboardDataView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        stok_per_kategori = (
+            Barang.objects
+            .values('kategori')
+            .annotate(total_stok=Sum('stok'))
+            .order_by('kategori')
+        )
+
+        data = [
+            {
+                'kategori': item['kategori'],
+                'total_stok': item['total_stok'] or 0,
+            }
+            for item in stok_per_kategori
+        ]
+
+        return JsonResponse({
+            'stok_per_kategori': data
+        })
--- warung_arum/urls.py
+++ warung_arum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('', include('usermanagement_24782039.urls')),
+    path('', include('dashboard_24782039.urls')),
 ]
