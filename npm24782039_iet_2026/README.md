9a144eca8e478af0df7012de1df7b7a8b4f77847
arumputriii <arummputriiww@gmail.com>
2026-06-10 14:28:19 +0700

    Tahap 1: Persiapan Sebelum Deployment

--- npm24782039_iet_2026/README.md
+++
@@ -1,2 +0,0 @@
-Npm: 24782039
-Nama: Arum Putri Wicaksana
--- npm24782039_iet_2026/usermanagement_24782039/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_admin', 'is_member')
-
-admin.site.register(CustomUser, CustomUserAdmin)
